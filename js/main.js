1165d2bc64cb1e74e5dc7536b50e4b21afcbcbdb
Cassie Lee <95990259+casslee8757@users.noreply.github.com>
2022-04-05 12:25:45 +1000

    Js navbar menu click

--- js/main.js
+++ js/main.js
@@ -4,12 +4,24 @@ const navbar = document.querySelector('#navbar')
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log('navbarheight',navbarHeight);
+   
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark')
     }else {
         navbar.classList.remove('navbar--dark')
 
     }
+})
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (e) => {
+    const target = e.target;
+    const link = target.dataset.link;
+    if(link == null){
+        return;
+    }
+    console.log(e.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth' })
 })
