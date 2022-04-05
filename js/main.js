d238f6c77d6be871d497ed6a9341bf2d72d828c1
Cassie Lee <95990259+casslee8757@users.noreply.github.com>
2022-04-05 12:11:10 +1000

    navbar transparent added

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+// Make navbar transparent when it is on the top 
+const navbar = document.querySelector('#navbar')
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+    console.log(window.scrollY);
+    console.log('navbarheight',navbarHeight);
+    if(window.scrollY > navbarHeight){
+        navbar.classList.add('navbar--dark')
+    }else {
+        navbar.classList.remove('navbar--dark')
+
+    }
+})
